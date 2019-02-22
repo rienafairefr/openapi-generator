fe984ca2deeefdb7b1d6697d8a86b5fbeaf5497c
rienafairefr <rienafairefr@gmail.com>
2019-02-22 12:55:13 +0100

    add _decode_utf8 to tornado & asyncio

--- samples/client/petstore/python-tornado/petstore_api/api/user_api.py
+++ samples/client/petstore/python-tornado/petstore_api/api/user_api.py
@@ -502,8 +502,8 @@ class UserApi(object):
         # multiple potential response types
         response_types = {
             200: 'User',
-            400: 'User',
-            404: 'User'
+            400: '',
+            404: ''
         }
 
         return self.api_client.call_api(
@@ -612,7 +612,7 @@ class UserApi(object):
         # multiple potential response types
         response_types = {
             200: 'str',
-            400: 'str'
+            400: ''
         }
 
         return self.api_client.call_api(
